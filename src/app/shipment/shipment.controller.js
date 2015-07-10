9716d7488ca0091daf72be0a42bcfc599d28007d
Wang Te <354061701@qq.com>
2015-07-10 17:59:05 +0800

    little change
    little change

--- src/app/shipment/shipment.controller.js
+++ src/app/shipment/shipment.controller.js
@@ -6,7 +6,13 @@
     .controller('ShipmentController', ShipmentController);
 
   /** @ngInject */
-  function ShipmentController($scope) {
+  function ShipmentController($scope, $http) {
   	$scope.shipments = ['货物1','货物2','货物3','货物4','货物5','货物6','货物7','货物8','货物9','货物10','货物11'];
+	// $http.get(Setting.url).success( function(response) {
+	// 	$scope.rqdata = response; 
+	// });
+	$.get(Setting.url, function(data,status){
+		console.log("hello");
+	});
   }
 })();
